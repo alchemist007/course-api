1eeb08c50214f1763b91cade43ecb1e566a0bf48
Nesh <hsenesh@gmail.com>
2017-06-30 16:46:26 -0400

    added put method

--- src/main/java/io/javabrains/springbootquickstart/TopicController.java
+++ src/main/java/io/javabrains/springbootquickstart/TopicController.java
@@ -1,9 +1,7 @@
 package io.javabrains.springbootquickstart;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -26,6 +24,16 @@ public class TopicController {
     @RequestMapping("/topics/{id}")
     public Topic getTopic(@PathVariable String id) {
         return topicService.getTopic(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/topics")
+    public void addTopic(@RequestBody Topic topic) {
+        topicService.addTopic(topic);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/topics{id}")
+    public void addTopic(@RequestBody Topic topic, @PathVariable String id) {
+        topicService.updateTopic(id, topic);
     }
 
 }
